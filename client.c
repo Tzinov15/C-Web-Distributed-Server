3b3fabd252e2bec0f729bd59068914d4186a98d9
Alex Tzinov <alex.tzinov@gmail.com>
2015-10-12 09:16:52 -0600

    update gitignore and client.c

--- client.c
+++ client.c
@@ -1,50 +1,84 @@
 #include "client.h"
 
 struct ClientFileContent {
-  char *method;
-  char *URI;
-  char *httpversion;
+  char *username;
+  char *password;
 };
 
 
+void deleteSubstring(char *original_string,const char *sub_string);
 void parse_client_conf_file(struct ClientFileContent *params, char *file_name);
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char ** argv) {
+  printf("| Welcome to this wonderful C client! |\n\n");
 
-  struct stat buffer;
   struct ClientFileContent client_params;
 
-  printf("| Welcome to this wonderful C client! |\n\n");
+
   if (argc < 2) {
     printf("Please specify a dfc.conf file\n");
     exit(1); }
-  if (stat (argv[1], &buffer) != 0) {
-    printf("File doesn't seem to exist...\n");
-    exit(1); }
-  else
-    printf("Yay file exists \n");
 
   parse_client_conf_file(&client_params, argv[1]);
 
+  /* Free the memory allocated for the strings within the ClientFileContent struct */
+  free (client_params.username);
+  free (client_params.password);
 }
 
 
 
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ * parse_client_conf_file - this function takes in a file name and the client params struct, and will popuate the struct after parsing and extracting info from the dfc.conf file 
+ *-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 void parse_client_conf_file(struct ClientFileContent *params, char *file_name) {
-  
   printf("Hello from parse_client_conf_file\n");
-  printf("This is the path of the file that was passed in:  %s\n", file_name);
+
+  /* Structs needed to be able to call the stat function which checks for file presence */
+  struct stat buffer;
+
+  /* leftover used in strtok to capture content after space, read_line is buffer that fgets writes to */
+  char *leftover, read_line[200];
+
+  /* file pointer for our dfc.conf file */
   FILE *config_file;
-  char *leftover;
+
+  if (stat (file_name, &buffer) != 0) {
+    perror("Conf file doesn't exist: ");
+    exit(-1);
+  }
+
   config_file = fopen(file_name, "r");
-  char read_line[200];
+  if (config_file == NULL) {
+    perror("Opening conf file: ");
+    exit(-1);
+  }
 
   while (fgets (read_line,200, config_file) != NULL) {
-    printf("%s \n", read_line);
+    /* Extract the user name field */
     if(strstr(read_line, "Username") != NULL) {
       strtok_r(read_line, " ", &leftover);
-      printf("%s", leftover);
+      deleteSubstring(leftover, "\n");
+      params->username = malloc(strlen(leftover)+1);
+      strcpy(params->username,leftover);
+    }
+    /* Extract the password name field */
+    if(strstr(read_line, "Password") != NULL) {
+      strtok_r(read_line, " ", &leftover);
+      deleteSubstring(leftover, "\n");
+      params->password= malloc(strlen(leftover)+1);
+      strcpy(params->password,leftover);
     }
   }
+  fclose(config_file);
+}
+
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------
+ * deleteSubstring - this function is a helper function that is used to remove newline characters from the end of extracted strings
+ *------------------------------------------------------------------------------------------------------------------------------------------- */
+void deleteSubstring(char *original_string,const char *sub_string) {
+  while( (original_string=strstr(original_string,sub_string)) )
+    memmove(original_string,original_string+strlen(sub_string),1+strlen(original_string+strlen(sub_string)));
 }
