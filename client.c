ed1d8cb4039c718cdb32e7d0a437da0620407da0
Alex Tzinov <alex.tzinov@gmail.com>
2015-10-13 14:18:56 -0600

    added message construction with headers

--- client.c
+++ client.c
@@ -6,10 +6,11 @@ struct ClientFileContent {
 };
 
 
+void construct_put_message(char *filename, char *filesize, char *filecontent, struct ClientFileContent *params, char *final_message);
 void deleteSubstring(char *original_string,const char *sub_string);
 void parse_client_conf_file(struct ClientFileContent *params, char *file_name);
 int handle_get (char *get_command);
-int handle_put (char *put_command);
+int handle_put (char *put_command, struct ClientFileContent *params);
 void handle_list ();
 //void handle_list (char *list_of_files);
 
@@ -25,7 +26,6 @@ int main(int argc, char ** argv) {
 
   parse_client_conf_file(&client_params, argv[1]);
 
-  /* Free the memory allocated for the strings within the ClientFileContent struct */
 
 printf("*-----------------------------------------------*\n");
 printf("|  To see of a list of your files, enter LIST   |\n");
@@ -47,7 +47,7 @@ printf("*-----------------------------------------------*\n");
         handle_list();
       }
       if (strncmp(user_input, "PUT", strlen("PUT")) == 0) {
-        handle_put(user_input);
+        handle_put(user_input, &client_params);
       }
 
       if (strncmp(user_input, "GET", strlen("GET")) == 0) {
@@ -56,6 +56,7 @@ printf("*-----------------------------------------------*\n");
 
     }
   }
+  /* Free the memory allocated for the strings within the ClientFileContent struct */
   free (client_params.username);
   free (client_params.password);
 }
@@ -94,7 +95,7 @@ int handle_get (char *get_command) {
 /*-------------------------------------------------------------------------------------------------------
  * handle_put - this function will be responsible for uploading all the files available on the DFS servers
  *------------------------------------------------------------------------------------------------------- */
-int handle_put (char *put_command) {
+int handle_put (char *put_command, struct ClientFileContent *params) {
   /* Structs needed to be able to call the stat function which checks for file presence */
   struct stat buffer;
   /* file_name used in strtok_r to capture content after space, read_line is buffer that fgets writes to */
@@ -122,7 +123,7 @@ int handle_put (char *put_command) {
     return 1;
   }
   /*------------------------
-   * File Upload Operations
+   * File Consruction Operations
    *------------------------*/
   FILE *file_to_upload;
 
@@ -150,11 +151,9 @@ int handle_put (char *put_command) {
   char portion_four_filename[sizeof(generic_filename) + sizeof(file_name) + 1];
   memset(&portion_four_filename, 0, sizeof(portion_four_filename));
   snprintf(portion_four_filename, sizeof(portion_four_filename), generic_filename, 4, file_name);
-  printf("This is the filename of the fourth portion: %s\n", portion_four_filename);
+  printf("This is the filename of the fourth portion: %s\n\n", portion_four_filename);
 
-  FILE *file_portion_one = fopen(portion_one_filename, "w");
-  fprintf(file_portion_one, "Testing...\n");
-  fclose(file_portion_one);
+
 
   ssize_t file_size;
   ssize_t file_size_copy;
@@ -162,7 +161,6 @@ int handle_put (char *put_command) {
 
   file_size = buffer.st_size; 
   file_size_copy = file_size;
-
   printf("The size of the original file is %zd\n", file_size); 
 
   portion_one_size = file_size / 4;
@@ -181,11 +179,109 @@ int handle_put (char *put_command) {
   printf("The size of the fourth portion is %zd\n", portion_four_size); 
 
   file_to_upload = fopen(file_name, "r");
-  if (file_to_upload == NULL) {
-    perror("Opening user file: ");
-    exit(-1);
-  }
-  return 0;
+  if (file_to_upload == NULL) { perror("Opening user file: ");
+    exit(-1); }
+
+  FILE *file_portion_one = fopen("tzinov.txt", "w");
+
+  char portion_one_buffer [portion_one_size];
+  fread(portion_one_buffer, 1, portion_one_size, file_to_upload);
+  portion_one_buffer[portion_one_size] = '\0';
+  printf("this is the result of reading the first fourth of the file into the buffer: %s\n", portion_one_buffer);
+  fwrite(portion_one_buffer, 1, portion_one_size, file_portion_one);
+
+  char portion_two_buffer [portion_two_size];
+  fread(portion_two_buffer, 1, portion_two_size, file_to_upload);
+  portion_two_buffer[portion_two_size] = '\0';
+  printf("this is the result of reading the third fourth of the file into the buffer: %s\n", portion_two_buffer);
+  fwrite(portion_two_buffer, 1, portion_two_size, file_portion_one);
+
+  char portion_three_buffer [portion_three_size];
+  fread(portion_three_buffer, 1, portion_three_size, file_to_upload);
+  portion_three_buffer[portion_three_size] = '\0';
+  printf("this is the result of reading the second fourth of the file into the buffer: %s\n", portion_three_buffer);
+  fwrite(portion_three_buffer, 1, portion_three_size, file_portion_one);
+
+  char portion_four_buffer [portion_four_size];
+  fread(portion_four_buffer, 1, portion_four_size, file_to_upload);
+  portion_four_buffer[portion_four_size] = '\0';
+  printf("this is the result of reading the fourth fourth of the file into the buffer: %s\n", portion_four_buffer);
+  fwrite(portion_four_buffer, 1, portion_one_size, file_portion_one);
+
+  /*------------------------
+     * Socket Sending 
+   *------------------------*/
+    int sock1, sock2, sock3, sock4;
+    struct sockaddr_in server1;
+    struct sockaddr_in server2;
+    struct sockaddr_in server3;
+    struct sockaddr_in server4;
+
+    char message[100];
+    char portion_one_size_char[16], portion_two_size_char[16], portion_three_size_char[16], portion_four_size_char[16];
+    snprintf(portion_one_size_char, sizeof(portion_one_size_char), "%zu", portion_one_size);
+    snprintf(portion_two_size_char, sizeof(portion_two_size_char), "%zu", portion_two_size);
+    snprintf(portion_three_size_char, sizeof(portion_three_size_char), "%zu", portion_three_size);
+    snprintf(portion_four_size_char, sizeof(portion_four_size_char), "%zu", portion_four_size);
+
+
+    char portion_one_message[sizeof("PUT ") + sizeof(portion_one_filename) + 8 + sizeof(params->username) + sizeof(params->password) + portion_one_size];
+    char portion_two_message[sizeof("PUT ") + sizeof(portion_two_filename) + 8 + sizeof(params->username) + sizeof(params->password) + portion_two_size];
+    char portion_three_message[sizeof("PUT ") + sizeof(portion_three_filename) + 8 + sizeof(params->username) + sizeof(params->password) + portion_three_size];
+    char portion_four_message[sizeof("PUT ") + sizeof(portion_four_filename) + 8 + sizeof(params->username) + sizeof(params->password) + portion_four_size];
+
+    construct_put_message((char *)&portion_one_filename, (char *)&portion_one_size_char, (char *)&portion_one_buffer, params, (char *)&portion_one_message);
+    construct_put_message((char *)&portion_two_filename, (char *)&portion_two_size_char, (char *)&portion_two_buffer, params, (char *)&portion_two_message);
+    construct_put_message((char *)&portion_three_filename, (char *)&portion_three_size_char, (char *)&portion_three_buffer, params, (char *)&portion_three_message);
+    construct_put_message((char *)&portion_four_filename, (char *)&portion_four_size_char, (char *)&portion_four_buffer, params, (char *)&portion_four_message);
+
+    printf("!!!!!!!!!!!!!!!!!\n\n");
+    printf("The size of portion one message is %zu\n", sizeof(portion_one_message));
+    printf("The length of portion one message is %zu\n", strlen(portion_one_message));
+    printf("This is our portion one message so far: \n%s\n", portion_one_message);
+
+
+
+    /*
+    sock1 = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock1 == -1)
+      printf("Could not create socket\n");
+
+    server1.sin_addr.s_addr = INADDR_ANY;
+    server1.sin_family = AF_INET;
+    server1.sin_port = htons( 10001);
+
+    if (connect(sock1, (struct sockaddr *)&server1, sizeof(server1)) < 0){
+      perror("connect failed. Error\n");
+      return 1;
+    }
+    printf("connected\n");
+
+    while (1) {
+      printf("Enter message : \n");
+      scanf("%s" , message);
+
+      if ( send(sock1, message, strlen(message) , 0) < 0) {
+          puts ("send failed");
+          return 1;
+        }
+    }
+    */
+    return 0;
+
+}
+
+void construct_put_message(char *filename, char *filesize, char *filecontent, struct ClientFileContent *params, char *final_message) {
+    strcpy(final_message, "PUT ");
+    strcat(final_message, filename);
+    strcat(final_message, " ");
+    strcat(final_message, filesize);
+    strcat(final_message, "\n");
+    strcat(final_message, params->username);
+    strcat(final_message, "\n");
+    strcat(final_message, params->password);
+    strcat(final_message, "\n");
+    strcat(final_message, filecontent);
 }
 
 /*-------------------------------------------------------------------------------------------------------
